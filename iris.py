ad2183bcee191e69f10a5524526ff7d6cec68e70
Rebecca Turley <bexiturley@gmail.com>
2019-03-31 17:49:15 +0100

    First draft of Project 2019

---
+++ iris.py
@@ -0,0 +1,22 @@
+# Rebecca Turley, 2019-03-30
+# Fisher's Iris Dataset
+
+import numpy as np
+import pandas as pd
+import sklearn 
+data=np.genfromtxt('iris.csv', delimiter=',')
+data = pd.read_csv("iris.csv")
+columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
+
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+
+print(data)
+
+#type(iris)
+#print (data [0])
+# References:
+
+# https://www.shanelynn.ie/python-pandas-read_csv-load-data-from-csv-files/
