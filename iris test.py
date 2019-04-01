12dd84d7fb492db8c106ae8a223220849930a6cf
Rebecca Turley <bexiturley@gmail.com>
2019-04-01 19:19:06 +0100

    Calculated mean of columns iris dataset

--- iris test.py
+++ iris test.py
@@ -1,6 +1,56 @@
 # Rebecca Turley, 2019-03-30
 # Fisher's Iris Dataset
 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn 
+data=np.genfromtxt('iris.csv', delimiter=',')
+data = pd.read_csv('iris.csv', delimiter=',')
+columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
+
+#from sklearn.datasets import load_iris
+
+firstcol = data[:,0]
+meanfirstool = numpy.mean(data[:,0])
+print ("Average of first solumn is:", meanfirstool)
+#df = pd.read_csv (r'C:\Users\Rebecca\Desktop\pands-project\iris.csv')
+#mean1 = df['sepal_lenght'].mean()   
+
+#iris = load_iris()
+
+
+print(data)
+
+#type(iris)
+#print (data [0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Calculate the mean of each column
 #import pandas as pd
 #import numpy as np
--- iris.py
+++ iris.py
@@ -1,23 +1,33 @@
 # Rebecca Turley, 2019-03-30
 # Fisher's Iris Dataset
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sklearn 
+
 data=np.genfromtxt('iris.csv', delimiter=',')
-data = pd.read_csv("iris.csv")
+
 columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
 
-from sklearn.datasets import load_iris
 
-iris = load_iris()
+firstcol = data[:,0]
+meanfirstool = np.mean(data[:,0])
+print ("Average of first column is:", meanfirstool)
+
+seconcol = data[:,0]
+meansecontool = np.mean(data[:,1])
+print ("Average of second column is:", meansecontool)
+
+thrdcol = data[:,0]
+meanthrdtool = np.mean(data[:,2])
+print ("Average of third column is:", meanthrdtool)
+
+fourcol = data[:,0]
+meanfourtool = np.mean(data[:,3])
+print ("Average of fourth column is:", meanfourtool)
 
 
-print(data)
 
-#type(iris)
-#print (data [0])
+#print(data)
+
 # References:
 
 # https://www.shanelynn.ie/python-pandas-read_csv-load-data-from-csv-files/
